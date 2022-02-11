7606732b2ef42be35cfae44ca0868df713d11c89
Keith <kcrowe123@yahoo.com>
2022-02-11 16:41:20 -0600

    Initial Commit

---
+++ src/main/java/com/keith/jenkins/JenkinsDeployableApplication.java
@@ -0,0 +1,13 @@
+package com.keith.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeployableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDeployableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keith/jenkins/JenkinsDeployableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keith.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
